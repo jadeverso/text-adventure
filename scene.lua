4418110da5ab7a80bb8b570a3b8b17232bf74ea5
brataccas <pamalvez@hotmail.com>
2022-11-05 23:25:21 -0300

    update scenes

--- scene.lua
+++ scene.lua
@@ -1,16 +1,44 @@
+_G.Name = "Default"
+_G.Backpack = {
+    ['Flashlight'] = true,
+}
 
 local scenes <const> = {
     ['scene_1'] = function()
-        print('\27[32;1mJoana:\27[m Qual é o seu nome mesmo?')
-        print('hum...')
-        io.write('\27[32;1mJoana:\27[m Qual é o seu nome mesmo? \n> ')
-        _G.nome = io.read()
+        print(
+            [[
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec justo quam, fermentum eu vulputate et, sollicitudin in lorem. Morbi nec nunc volutpat, dignissim tellus non, vulputate nisi. Mauris bibendum metus dui, non dictum arcu interdum eu. Vivamus hendrerit libero mi, vitae accumsan est feugiat ac. Aenean convallis nibh gravida tempor facilisis. Donec mollis purus id lorem fermentum posuere. Fusce vel erat porta, tristique quam quis, mattis est. Pellentesque hendrerit commodo congue. Praesent in luctus neque. Suspendisse libero ligula, lobortis ut finibus vel, condimentum maximus diam.  
+
+Duis volutpat enim eget metus tristique facilisis. Maecenas bibendum metus in pulvinar pretium.
+
+]]
+            ..
+            '\27[32;1mJoana:\27[m '
+            ..
+            [[
+Seu nome, por favor?]]
+        )
+        io.write('\n> ')
+        Name = io.read()
         return true;
     end,
     ['scene_2'] = function()
-        print('\27[32;1mJoana:\27[m O '..nome)
+        
+        if (Backpack['Flashlight']) then
+            print('You have a flashlight.')
+        end
+
+        print('\27[32;1mJoana:\27[m O ' .. Name)
+        return true;
+    end,
+    ['scene_3'] = function()
+        print('\27[32;1mJoana:\27[m '
+            ..
+            [[
+Encerrou o jogo]]
+        )
         return false;
-    end
+    end,
 }
 
 local M <const> = {}
